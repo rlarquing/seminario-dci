be9d79a2afda25e6817024281b66b9ba56f0437e
Z User <z@container>
2026-03-11 04:34:28 +0000

    Corregir nombre de importación PrismaLibSql

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
@@ -15,7 +15,7 @@ function createPrismaClient() {
       authToken: process.env.TURSO_AUTH_TOKEN,
     })
     
-    const adapter = new PrismaLibSQL(libsql)
+    const adapter = new PrismaLibSql(libsql)
     
     return new PrismaClient({
       adapter,
